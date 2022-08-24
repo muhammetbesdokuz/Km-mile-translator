f13a9583491d6d35cab4ba77be73b2c87ef6d22c
muhammetbesdokuz <muhammetbesdokuz@gmail.com>
2022-08-24 20:53:34 +0300

    Create KacMileEder.py

---
+++ KacMileEder.py
@@ -0,0 +1,7 @@
+﻿#ask the user to enter a km value, convert the user entered km to miles.
+
+ConversionRate= 0.62 
+km= int(input("how much km's?"))
+
+mile= km * ConversionRate
+print(str(km)+"km="+str(mile)+" miles")
